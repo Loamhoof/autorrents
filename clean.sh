43b685e15895943a312a98fda032798a6b298757
MEDARD Soeren <soerenm@theodo.fr>
2016-05-28 14:39:38 +0200

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,1 +1,1 @@
-transmission-remote -l | grep Done | grep -oP "\d+(?= +100%)" | xargs -n 1 -I % transmission-remote  -t% -r
+transmission-remote -l | grep -oP "\d+(?= +100%)" | xargs -n 1 -I % transmission-remote  -t% -r
